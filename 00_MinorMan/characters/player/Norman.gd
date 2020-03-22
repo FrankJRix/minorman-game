5e0df9c0540e4ea78bda994b73de2f0846bbc7ef
FrankJ <franco.risi94@hotmail.it>
2020-03-22 18:57:10 +0100

    Animation revamp: partone.

--- 00_MinorMan/characters/player/Norman.gd
+++ 00_MinorMan/characters/player/Norman.gd
@@ -2,17 +2,27 @@ extends KinematicBody2D
 
 const MAX_SPEED = 500
 const SPRINT_MULTIPLIER = 1.5
+const WEST_TRESHOLD = PI * 7/8
+const SW_TRESHOLD = PI * 5/8
+const SOUTH_TRESHOLD = PI * 3/8
+const SE_TRESHOLD = PI * 1/8
+const EAST_TRESHOLD = PI * -1/8
+const NE_TRESHOLD = PI * -3/8
+const NORTH_TRESHOLD = PI * -5/8
+const NW_TRESHOLD = PI * -7/8
 
 var speed = 0
 var velocity = Vector2()
 var direction = Vector2()
 var multi = 1
+var relative_mouse_angle = 0
 
 func _physics_process(delta):
 	move()
 
 func move():
 	get_input()
+	manage_animation()
 
 func get_input():
 	multi = 1
@@ -23,18 +33,43 @@ func get_input():
 	
 	if Input.is_action_pressed("move_north"):
 		direction.y -= 1
-		$AnimationPlayer.play("idle_north")
 	if Input.is_action_pressed("move_south"):
 		direction.y += 1
-		$AnimationPlayer.play("idle_south")
 	if Input.is_action_pressed("move_west"):
 		direction.x -= 1
-		$AnimationPlayer.play("idle_west")
 	if Input.is_action_pressed("move_east"):
 		direction.x += 1
-		$AnimationPlayer.play("idle_east")
 	
 	direction = direction.normalized()
 	
 	velocity = direction * MAX_SPEED * multi
 	move_and_slide(velocity)
+
+func manage_animation():
+	relative_mouse_angle = get_local_mouse_position().angle()
+	print(get_local_mouse_position().angle())
+	
+	if relative_mouse_angle >= WEST_TRESHOLD:
+		$AnimationPlayer.play("idle_west")
+	elif relative_mouse_angle >= SW_TRESHOLD:
+		$AnimationPlayer.play("idle_sw")
+	elif relative_mouse_angle >= SOUTH_TRESHOLD:
+		$AnimationPlayer.play("idle_south")
+	elif relative_mouse_angle >= SE_TRESHOLD:
+		$AnimationPlayer.play("idle_se")
+	elif relative_mouse_angle >= EAST_TRESHOLD:
+		$AnimationPlayer.play("idle_east")
+	elif relative_mouse_angle >= NE_TRESHOLD:
+		$AnimationPlayer.play("idle_ne")
+	elif relative_mouse_angle >= NORTH_TRESHOLD:
+		$AnimationPlayer.play("idle_north")
+	elif relative_mouse_angle >= NW_TRESHOLD:
+		$AnimationPlayer.play("idle_nw")
+	else:
+		$AnimationPlayer.play("idle_west")
+
+
+
+
+
+
