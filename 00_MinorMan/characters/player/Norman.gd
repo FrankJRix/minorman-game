b37862cc037144bf7d1bc366866d0f172b09c423
FrankJ <franco.risi94@hotmail.it>
2020-03-28 17:53:59 +0100

    Preparate le preparazioni alchemiche, manca da implementare la fsm.

--- 00_MinorMan/characters/player/Norman.gd
+++ 00_MinorMan/characters/player/Norman.gd
@@ -12,6 +12,8 @@ const NORTH_TRESHOLD = PI * -5/8
 const NW_TRESHOLD = PI * -7/8
 const EPSILON = 0.01
 
+enum MOUSE_SECTOR {WEST, SOUTHWEST, SOUTH, SOUTHEAST, EAST, NORTHEAST, NORTH, NORTHWEST}
+
 var speed = 0
 var velocity = Vector2()
 var direction = Vector2()
@@ -49,24 +51,23 @@ func get_input():
 func manage_animation():
 	relative_mouse_angle = get_local_mouse_position().angle()
 	
-	if relative_mouse_angle >= WEST_TRESHOLD:
-		$SpriteSheetAnim.play("idle_west")
-	elif relative_mouse_angle >= SW_TRESHOLD:
-		$SpriteSheetAnim.play("idle_sw")
-	elif relative_mouse_angle >= SOUTH_TRESHOLD:
-		$SpriteSheetAnim.play("idle_south")
-	elif relative_mouse_angle >= SE_TRESHOLD:
-		$SpriteSheetAnim.play("idle_se")
-	elif relative_mouse_angle >= EAST_TRESHOLD:
-		$SpriteSheetAnim.play("idle_east")
-	elif relative_mouse_angle >= NE_TRESHOLD:
-		$SpriteSheetAnim.play("idle_ne")
-	elif relative_mouse_angle >= NORTH_TRESHOLD:
-		$SpriteSheetAnim.play("idle_north")
-	elif relative_mouse_angle >= NW_TRESHOLD:
-		$SpriteSheetAnim.play("idle_nw")
-	else:
-		$SpriteSheetAnim.play("idle_west")
+	match check_mouse_sector(relative_mouse_angle):
+		MOUSE_SECTOR.WEST:
+			$SpriteSheetAnim.play("idle_west")
+		MOUSE_SECTOR.SOUTHWEST:
+			$SpriteSheetAnim.play("idle_sw")
+		MOUSE_SECTOR.SOUTH:
+			$SpriteSheetAnim.play("idle_south")
+		MOUSE_SECTOR.SOUTHEAST:
+			$SpriteSheetAnim.play("idle_se")
+		MOUSE_SECTOR.EAST:
+			$SpriteSheetAnim.play("idle_east")
+		MOUSE_SECTOR.NORTHEAST:
+			$SpriteSheetAnim.play("idle_ne")
+		MOUSE_SECTOR.NORTH:
+			$SpriteSheetAnim.play("idle_north")
+		MOUSE_SECTOR.NORTHWEST:
+			$SpriteSheetAnim.play("idle_nw")
 	
 	if direction.length() > EPSILON:
 		$CutoutAnim.play("move")
@@ -74,7 +75,29 @@ func manage_animation():
 		$CutoutAnim.play("idle")
 
 
-
+func check_mouse_sector(mouse_angle):
+	var sector
+	
+	if relative_mouse_angle >= WEST_TRESHOLD:
+		sector = MOUSE_SECTOR.WEST
+	elif relative_mouse_angle >= SW_TRESHOLD:
+		sector = MOUSE_SECTOR.SOUTHWEST
+	elif relative_mouse_angle >= SOUTH_TRESHOLD:
+		sector = MOUSE_SECTOR.SOUTH
+	elif relative_mouse_angle >= SE_TRESHOLD:
+		sector = MOUSE_SECTOR.SOUTHEAST
+	elif relative_mouse_angle >= EAST_TRESHOLD:
+		sector = MOUSE_SECTOR.EAST
+	elif relative_mouse_angle >= NE_TRESHOLD:
+		sector = MOUSE_SECTOR.NORTHEAST
+	elif relative_mouse_angle >= NORTH_TRESHOLD:
+		sector = MOUSE_SECTOR.NORTH
+	elif relative_mouse_angle >= NW_TRESHOLD:
+		sector = MOUSE_SECTOR.NORTHWEST
+	else:
+		sector = MOUSE_SECTOR.WEST
+	
+	return sector
 
 
 
