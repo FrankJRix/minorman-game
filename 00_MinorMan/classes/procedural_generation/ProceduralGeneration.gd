d8678406d5061ce2159953f68322020fb3e8975e
FrankJ <franco.risi94@hotmail.it>
2020-04-09 22:22:45 +0200

    La porta. S'è aperta.

--- 00_MinorMan/classes/procedural_generation/ProceduralGeneration.gd
+++ 00_MinorMan/classes/procedural_generation/ProceduralGeneration.gd
@@ -28,9 +28,7 @@ var tile
 
 func _ready():
 	randomize()
-	initialize_map()
-	#read_map()
-	build_cave_map()
+	setup()
 
 
 func initialize_map():
@@ -147,19 +145,49 @@ func set_tile_id(tile_pos, id):
 	map[tile_pos.x][tile_pos.y]["tunnel_id"] = id
 
 
+func spawn_player():
+	var point_on_map = fetch_spawn_point()
+	var point_in_world = tilemap.map_to_world(point_on_map) * tilemap.scale.x
+	
+	print(point_on_map)
+	print(point_in_world)
+	
+	$YSort/Norman.position = point_in_world
+
+
+func fetch_spawn_point():
+	var ideal_spawn
+	
+	for i in CAVE_WIDTH:
+		for j in CAVE_HEIGHT:
+			if map[i][j]["tunnel_id"] == largest_tunnel["id"] and count_neighbourhood(i, j) == 0:
+				ideal_spawn = Vector2(i, j)
+				break
+	
+	if  not ideal_spawn:
+		print("Could not find suitable spawn point.")
+		ideal_spawn = Vector2(-20, -20)
+	
+	return ideal_spawn
+
+
 func build_cave_map():
 	tilemap.build_map(map, CAVE_WIDTH, CAVE_HEIGHT)
 
 
+func setup():
+	initialize_map()
+	for i in NUMBER_OF_STEPS:
+		cellular_automaton_step()
+	identify_tunnels()
+	evaluate()
+	build_cave_map()
+	spawn_player()
+
+
 func _input(event):
 	if Input.is_action_just_pressed("test_input"):
-		#clear_map()
-		for i in NUMBER_OF_STEPS:
-			cellular_automaton_step()
-		identify_tunnels()
-		evaluate()
-		#read_map()
-		build_cave_map()
+		setup()
 
 
 #######################################################################################EOF
---
+++ 00_MinorMan/levels/Door.gd
@@ -0,0 +1,20 @@
+extends Area2D
+# TEST ONLY! Questo è un hack diel cazzo messo insieme al volo, è lammerda!
+
+func _ready():
+	set_process_unhandled_input(false)
+
+
+func _on_Door_body_entered(body):
+	if body.name == "Norman":
+		set_process_unhandled_input(true)
+
+
+func _on_Door_body_exited(body):
+	if body.name == "Norman":
+		set_process_unhandled_input(false)
+
+
+func _unhandled_input(event):
+	if Input.is_action_just_pressed("interact"):
+		get_tree().change_scene("res://classes/procedural_generation/ProceduralGeneration.tscn")
