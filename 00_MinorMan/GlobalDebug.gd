0d562c09c5cb5a435d815d714b48c756f0a86736
FrankJ <franco.risi94@hotmail.it>
2020-05-17 16:46:37 +0200

    Aggiunto autodetect joypad al lancio.

--- 00_MinorMan/GlobalDebug.gd
+++ 00_MinorMan/GlobalDebug.gd
@@ -1,12 +1,29 @@
 extends Node
 
 enum TargetMode {MOUSE, JOYPAD}
-var mode: int = TargetMode.MOUSE
+var mode = TargetMode.MOUSE
+
+func _ready():
+	if Input.get_connected_joypads():
+		_set_target_mode_joypad()
+	else:
+		_set_target_mode_mouse()
 
 func _input(event):
 	if Input.is_action_just_released("quit"):
 		get_tree().quit()
 	if event.is_action_pressed("test_fullscreen"):
 		OS.set_window_fullscreen(not OS.is_window_fullscreen())
-	if Input.is_action_pressed("toggle_target_mode"):
-		mode = (mode + 1) % 2
+	if Input.is_action_just_released("toggle_target_mode"):
+		if mode == TargetMode.MOUSE:
+			_set_target_mode_joypad()
+		elif mode == TargetMode.JOYPAD:
+			_set_target_mode_mouse()
+
+func _set_target_mode_joypad():
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	mode = TargetMode.JOYPAD
+
+func _set_target_mode_mouse():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	mode = TargetMode.MOUSE
